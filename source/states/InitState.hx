bc7dd6633e722ef1b0a9418269d8ecc512357093
chungus <chungus@big.com>
2026-04-09 21:32:10 +0200

    Cataclysm! Lua wrecked!

---
+++ source/states/InitState.hx
@@ -0,0 +1,114 @@
+package states;
+
+import backend.WeekData;
+import backend.Highscore;
+import flixel.input.keyboard.FlxKey;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.graphics.frames.FlxFrame;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
+import haxe.Json;
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import shaders.ColorSwap;
+import states.StoryMenuState;
+import states.OutdatedState;
+import states.MainMenuState;
+
+typedef TitleData =
+{
+	titlex:Float,
+	titley:Float,
+	startx:Float,
+	starty:Float,
+	gfx:Float,
+	gfy:Float,
+	backgroundSprite:String,
+	bpm:Float
+}
+
+class InitState extends MusicBeatState
+{
+	override public function create():Void
+	{
+		Paths.clearStoredMemory();
+
+		#if LUA_ALLOWED
+		Mods.pushGlobalMods();
+		#end
+		Mods.loadTopMod();
+
+		FlxG.fixedTimestep = false;
+		FlxG.game.focusLostFramerate = 60;
+		FlxG.keys.preventDefaultKeys = [TAB];
+
+		super.create();
+
+		FlxG.save.bind('funkin', CoolUtil.getSavePath());
+
+		ClientPrefs.loadPrefs();
+
+		#if CHECK_FOR_UPDATES
+		if (ClientPrefs.data.checkForUpdates && !closedState)
+		{
+			trace('checking for update');
+			var http = new haxe.Http("https://raw.githubusercontent.com/ShadowMario/FNF-PsychEngine/main/gitVersion.txt");
+
+			http.onData = function(data:String)
+			{
+				updateVersion = data.split('\n')[0].trim();
+				TitleState.updateVersion = updateVersion.trim();
+				var curVersion:String = MainMenuState.psychEngineVersion.trim();
+				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
+				if (updateVersion != curVersion)
+				{
+					trace('versions arent matching!');
+					mustUpdate = true;
+					FlxG.switchState(new OutdatedState());
+					return;
+				}
+			}
+
+			http.onError = function(error)
+			{
+				trace('error: $error');
+			}
+
+			http.request();
+		}
+		#end
+
+		Highscore.load();
+
+		if (FlxG.save.data != null && FlxG.save.data.fullscreen)
+		{
+			FlxG.fullscreen = FlxG.save.data.fullscreen;
+			// trace('LOADED FULLSCREEN SETTING!!');
+		}
+		persistentUpdate = true;
+		persistentDraw = true;
+
+		if (FlxG.save.data.weekCompleted != null)
+		{
+			StoryMenuState.weekCompleted = FlxG.save.data.weekCompleted;
+		}
+
+		FlxG.mouse.visible = false;
+		#if FREEPLAY
+		MusicBeatState.switchState(new FreeplayState());
+		#elseif CHARTING
+		MusicBeatState.switchState(new ChartingState());
+		#else
+		if (FlxG.save.data.flashing == null && !FlashingState.leftState)
+		{
+			FlxTransitionableState.skipNextTransIn = true;
+			FlxTransitionableState.skipNextTransOut = true;
+			MusicBeatState.switchState(new FlashingState());
+			return;
+		}
+		#end
+		FlxG.switchState(new TitleState());
+	}
+}
